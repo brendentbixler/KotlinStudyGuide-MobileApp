0a777a617a54dd98563f1bca9e6efb7d942aed81
brendentbixler <brendenbixler@u.boisestate.edu>
2022-08-02 15:48:05 -0600

    Bug fix - default menu item selected. Feature - added 5th menu item

--- app/src/main/java/com/example/kotlinstudyguide/MainActivity.kt
+++ app/src/main/java/com/example/kotlinstudyguide/MainActivity.kt
@@ -4,10 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.kotlinstudyguide.databinding.ActivityMainBinding
-import com.example.kotlinstudyguide.fragments.AndroidFragment
-import com.example.kotlinstudyguide.fragments.HomeFragment
-import com.example.kotlinstudyguide.fragments.KotlinFragment
-import com.example.kotlinstudyguide.fragments.ThirdPartyFragment
+import com.example.kotlinstudyguide.fragments.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +13,7 @@ class MainActivity : AppCompatActivity() {
     private val androidFragment = AndroidFragment()
     private val thirdPartyFragment = ThirdPartyFragment()
     private val homeFragment = HomeFragment()
+    private val newFragment = NewFragment()
 
     private lateinit var binding : ActivityMainBinding
 
@@ -24,14 +22,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // set default icon
+        val bottomNavigationView: BottomNavigationView = findViewById(R.id.bottom_navigation)
+        bottomNavigationView.selectedItemId = R.id.ic_home
+
+        // set initial fragment
         replaceFragment(homeFragment)
 
         binding.bottomNavigation.setOnItemSelectedListener {
             when(it.itemId) {
-                R.id.ic_kotlin -> replaceFragment(kotlinFragment)
                 R.id.ic_home -> replaceFragment(homeFragment)
+                R.id.ic_kotlin -> replaceFragment(kotlinFragment)
                 R.id.ic_android -> replaceFragment(androidFragment)
                 R.id.ic_third_party -> replaceFragment(thirdPartyFragment)
+                R.id.ic_news -> replaceFragment(newFragment)
             }
             true
         }
@@ -40,7 +44,7 @@ class MainActivity : AppCompatActivity() {
 
     private fun replaceFragment(fragment: Fragment) {
         val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragment_container, fragment)
+        transaction.replace(R.id.fragment_container, fragment, fragment.tag)
         transaction.commit()
 
     }
